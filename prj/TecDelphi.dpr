62b0a6fd4d6b981b2dfe5b89063d01a8917d887d
ruisjr2005 <ruisjr2005@gmail.com>
2026-09-18 11:19:32 -0300

    #015 - Commit com ajustes no dpr e dfm do view Pedido

--- prj/TecDelphi.dpr
+++ prj/TecDelphi.dpr
@@ -3,9 +3,9 @@ program TecDelphi;
 uses
   Vcl.Forms,
   View.Pedido in '..\src\view\View.Pedido.pas' {frmPedido},
-  Model.cliente in '..\src\model\Model.cliente.pas',
-  Model.produto in '..\src\model\Model.produto.pas',
-  Model.pedido in '..\src\model\Model.pedido.pas',
+  Model.Cliente in '..\src\model\Model.Cliente.pas',
+  Model.Produto in '..\src\model\Model.Produto.pas',
+  Model.Pedido in '..\src\model\Model.Pedido.pas',
   Repositorio.Interfaces in '..\src\repositorio\Repositorio.Interfaces.pas',
   Core.Database.Criteria in '..\src\repositorio\Core\Database\Core.Database.Criteria.pas',
   Core.Database.DBConnectionFDAdapter in '..\src\repositorio\Core\Database\Core.Database.DBConnectionFDAdapter.pas',
@@ -21,7 +21,12 @@ uses
   Core.Environment in '..\src\repositorio\Core\Core.Environment.pas',
   Core.Exceptions in '..\src\repositorio\Core\Core.Exceptions.pas',
   Repositorio.Pedido in '..\src\repositorio\Repositorio.Pedido.pas',
-  Servico.Pedido in '..\src\service\Servico.Pedido.pas';
+  Servico.Pedido in '..\src\service\Servico.Pedido.pas',
+  Servico.Cliente in '..\src\service\Servico.Cliente.pas',
+  Servico.Produto in '..\src\service\Servico.Produto.pas',
+  Repositorio.Cliente in '..\src\repositorio\Repositorio.Cliente.pas',
+  Repositorio.Produto in '..\src\repositorio\Repositorio.Produto.pas',
+  Servico.Base in '..\src\service\Servico.Base.pas';
 
 {$R *.res}
 
