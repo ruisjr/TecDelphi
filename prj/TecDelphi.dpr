46bcb310f1976bfc0c334cc1cb2a32dc601f8024
ruisjr2005 <ruisjr2005@gmail.com>
2026-09-18 02:09:16 -0300

    #010 - Commit dos demais arquivos do projeto Delphi

--- prj/TecDelphi.dpr
+++ prj/TecDelphi.dpr
@@ -3,11 +3,25 @@ program TecDelphi;
 uses
   Vcl.Forms,
   View.Pedido in '..\src\view\View.Pedido.pas' {frmPedido},
-  model.cliente in '..\src\model\model.cliente.pas',
-  model.produto in '..\src\model\model.produto.pas',
-  model.pedido in '..\src\model\model.pedido.pas',
-  repositorio.interfaces in '..\src\repositorio\repositorio.interfaces.pas',
-  repositorio.database.firedac in '..\src\repositorio\repositorio.database.firedac.pas';
+  Model.cliente in '..\src\model\Model.cliente.pas',
+  Model.produto in '..\src\model\Model.produto.pas',
+  Model.pedido in '..\src\model\Model.pedido.pas',
+  Repositorio.Interfaces in '..\src\repositorio\Repositorio.Interfaces.pas',
+  Core.Database.Criteria in '..\src\repositorio\Core\Database\Core.Database.Criteria.pas',
+  Core.Database.DBConnectionFDAdapter in '..\src\repositorio\Core\Database\Core.Database.DBConnectionFDAdapter.pas',
+  Core.Database.DBManager in '..\src\repositorio\Core\Database\Core.Database.DBManager.pas',
+  Core.Database.DBQueryFDAdapter in '..\src\repositorio\Core\Database\Core.Database.DBQueryFDAdapter.pas',
+  Core.Database.DBRtti in '..\src\repositorio\Core\Database\Core.Database.DBRtti.pas',
+  Core.Database.DBTypes in '..\src\repositorio\Core\Database\Core.Database.DBTypes.pas',
+  Core.Database.Interfaces in '..\src\repositorio\Core\Database\Core.Database.Interfaces.pas',
+  Core.Database.RttiHelper in '..\src\repositorio\Core\Database\Core.Database.RttiHelper.pas',
+  Core.Database.SQLMaker in '..\src\repositorio\Core\Database\Core.Database.SQLMaker.pas',
+  Core.Entidade.CustomAttributes in '..\src\repositorio\Core\Entidades\Core.Entidade.CustomAttributes.pas',
+  Core.Entidade.ModelBase in '..\src\repositorio\Core\Entidades\Core.Entidade.ModelBase.pas',
+  Core.Environment in '..\src\repositorio\Core\Core.Environment.pas',
+  Core.Exceptions in '..\src\repositorio\Core\Core.Exceptions.pas',
+  Repositorio.Pedido in '..\src\repositorio\Repositorio.Pedido.pas',
+  Servico.Pedido in '..\src\service\Servico.Pedido.pas';
 
 {$R *.res}
 
